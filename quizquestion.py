4f4c1197ae0c28d1c2ad4607fe9b9bae94a0d05a
JC (Joshua Caleb) Dy <jcdy@berkeley.edu>
2019-08-10 14:10:43 -0700

    Basic functionality for question checking working

--- quizquestion.py
+++ quizquestion.py
@@ -6,21 +6,26 @@ class QuizQuestion:
 		self.question = question
 		self.answer = answer
 
-	def question(self):
+	def get_question(self):
 		"""Getter method for the question text."""
 		return self.question
 
-	def answer(self):
+	def get_answer(self):
 		"""Getter method for the answer text."""
 		return self.answer
 
-	def __repr__(self):
-		return "Question: {0} Answer: {1}\n".format(self.question, self.answer)
+	# def __repr__(self):
+	# 	return "Question: {0} Answer: {1}\n".format(self.question, self.answer)
 
 	@staticmethod
 	def get_game_questions(indices):
-		"""Returns a list of QuizQuestions at the specified indices."""
-		return [questions_all[ind] for ind in indices]
+		"""Returns a JSON compatible list of QuizQuestions at the specified indices."""
+		game_questions = []
+		for ind in indices:
+			question_text = questions_all[ind].get_question()
+			answer_text = questions_all[ind].get_answer()
+			game_questions.append([question_text, answer_text])
+		return game_questions
 
 
 # Idea: At the beginning of the game, users can choose a country/region to focus questions on
@@ -42,3 +47,4 @@ questions_other = [
 
 questions_all = questions_england + questions_france + questions_germany + questions_other
 questions_all = [QuizQuestion(q, a) for q, a in questions_all]
+print(QuizQuestion.get_game_questions([1]))
