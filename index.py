320f6458eebfb8ebfaa8173288386465d8ad992e
JC (Joshua Caleb) Dy <jcdy@berkeley.edu>
2019-08-10 13:05:06 -0700

    Export quiz questions to separate file

--- index.py
+++ index.py
@@ -1,4 +1,6 @@
 import response_builder
+import quizquestion
+import random
 
 # Handler for launch, intent, and session ended Requests. 
 # This function routes the incoming request based on type (LaunchRequest,
@@ -12,6 +14,7 @@ def handler(event, context):
         return on_intent_request(event)
     elif event['request']['type'] == "SessionEndedRequest":
         return on_session_end()
+    return on_intent_request(event)
 
 # Called when the session starts.
 def on_session_start():
@@ -24,7 +27,10 @@ def on_launch(event):
     reprompt_message = "Try to get as many questions right as you can."
     card_text = "Respond with your best guess for each question."
     card_title = "Welcome to WhereInTheWorld!"
-    return response_builder.build_json_response(welcome_message, card_text, card_title, reprompt_message, False)
+
+    game_info = {"questions" : populate_game_questions(), "current_q_index" : 0, "score" : 0}
+    welcome_message += game_info["questions"][0][0]
+    return response_builder.build_json_response(welcome_message, card_text, card_title, reprompt_message, game_info, False)
 
 def on_session_end():
     print("Session Ended.")
@@ -32,7 +38,40 @@ def on_session_end():
 # Called when a user input is mapped by Alexa to an intent
 def on_intent_request(event):
     intent_name = event['request']['intent']['name']
+    if intent_name == "AnswerIntent":
+    	return handleAnswerRequest(event)
+    # elif intentName == "DontKnowIntent":
+    #     handleDontKnowRequest(event)
+
+def handleAnswerRequest(event):
+	user_answer = event['request']['intent']['slots']['Capital']['value']
+	all_game_questions = event['session']['attributes']['questions']
+	
+	question_number = event['session']['attributes']["current_q_index"]
+	if user_answer == all_game_questions[question_number][1]:
+		event['session']['attributes']["score"] += 1
+		return response_builder.build_json_response(user_answer + " is correct!", "","","", "", False)
+	else:
+		return response_builder.build_json_response(user_answer + " is incorrect! You suck.", "","","", "", False)
+	#next_question = all_game_questions[question_number + 1]
+	
+	
+	#if user_answer == all_game_questions[question_number][1]:
+		#event['sessionAttributes']["score"] += 1
+		#return response_builder.build_json_response(user_answer + " is correct!" + next_question, "","","", event['session']['attributes'], False)
+	#eturn response_builder.build_json_response(user_answer + " is incorrect!" + next_question, "","","", event['session']['attributes'], False)
+
+def handleDontKnowRequest(event):
+	pass
 
 # SKILL SPECIFIC LOGIC
 
+
 NUM_GAME_QUESTIONS = '5';
+
+def populate_game_questions():
+  """Build and return the list of questions for this game."""
+  indices = random.sample(range(0, len(quizquestion.questions_all)), 5) # If user doesn't specify, choose 5 random questions
+  return quizquestion.QuizQuestion.get_game_questions(indices)   
+
+print(populate_game_questions()) 
---
+++ quizquestion.py
@@ -0,0 +1,33 @@
+class QuizQuestion:
+	"""Represents a quiz question."""
+
+	def __init__(self, question, answer):
+		"""Create a new quiz question with its answer. """
+		self.question = question
+		self.answer = answer
+
+	def question(self):
+		"""Getter method for the question text."""
+		return self.question
+
+	def answer(self):
+		"""Getter method for the answer text."""
+		return self.answer
+
+	def __repr__(self):
+		return "Question: {0} Answer: {1}\n".format(self.question, self.answer)
+
+	@staticmethod
+	def get_game_questions(indices):
+		"""Returns a list of QuizQuestions at the specified indices."""
+		return [questions_all[ind] for ind in indices]
+
+
+# Idea: At the beginning of the game, users can choose a country/region to focus questions on
+questions_england = [("What is the capital of the UK?", "London"), ("In what country can you find a clock tower called the Big Ben?", "UK")]
+questions_france = [("What is the capital of France?", "Paris"), ("In which city is the Eiffel Tower located?", "Paris")]
+questions_germany = [("What is the capital of Germany", "Berlin"), ("Where in the world can you find the Cologne Cathedral?", "Germany")]
+questions_other = [("Which of the following countries has the highest population? France, Germany, Italy?", "Germany")]
+
+questions_all = questions_england + questions_france + questions_germany + questions_other
+questions_all = [QuizQuestion(q, a) for q, a in questions_all]
